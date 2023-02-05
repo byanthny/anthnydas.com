22a66f9905fd520b02ea994f441a2be03d4b371e
Anthony Das <dev@anthnydas.com>
2023-02-04 20:30:18 -0500

    tailwind :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://anthnydas.com",
+  integrations: [tailwind()]
 });
